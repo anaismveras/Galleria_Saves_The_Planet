4d4ed56ab8eab2749e96dd91d82ba1cef3e31fa4
anaismveras <verasmanais@gmail.com>
2021-10-24 10:10:51 -0400

    added css and started js for game fucntion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// console.log('Hello World')
+const game = document.getElementById('canvas')
+const trashPoints = document.getElementById('trash')
+const timeGame = document.getElementById('time')
+const ctx = game.getContext('2d')
+
+function hero (x, y, color, width, height) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.height = height
+    this.width = width
+    this.alive = true
+    this.render = function() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+}
+}
+let galleria = new hero(10, 10, '#FFFFFF', 20, 100)
+console.log('this is hero', galleria)
+
